a461451145a70a4ce3433c5c81ef1df3bbbd222e
Daniil Zinenko <danil@nulana.com>
2017-09-29 18:49:16 +0300

    sync

--- test/check/prepare.py
+++ test/check/prepare.py
@@ -2,14 +2,22 @@
 import sys
 import re
 
-if len(sys.argv) != 2:
-    print('format: ./prepare.py <file.answ>')
+r = re.compile(r'\d+ \d+ \d+ \d+ \d+\n(GET|POST) (\S+) HTTP/1\.1\nHost: travels\.com\nUser-Agent: Technolab/1\.0 \(Docker; CentOS\) Highload/1\.0\nAccept: \*/\*\nConnection: keep-alive\n\n\nHTTP/1\.1 (\d+).+\nServer: fasthttp\nDate: .+\n(Content-Type: text/plain; charset=utf-8\nContent-Length: \d+\n|Content-Length: \d+\n)\n(.*)\n', re.MULTILINE)
+
+if len(sys.argv) != 3:
+    print('format: ./prepare.py <in.answ> <out.answ>')
     exit(1)
 
-f0 = open(sys.argv[1], 'r')
-f0_lines = []
-for line in f0:
-    f0_lines.append(re.sub(r'\d+ \d+ \d+ \d+ \d+\n(GET|POST) (\S+) HTTP/1\.1\nHost: travels\.com\nUser-Agent: Technolab/1\.0 \(Docker; CentOS\) Highload/1\.0\nAccept: \*/\*\nConnection: keep-alive\n\n\nHTTP/1\.1 (\d+).+\nServer: fasthttp\nDate: .+\n(Content-Type: text/plain; charset=utf-8\nContent-Length: \d+\n|Content-Length: \d+\n)\n(.*)\n', r'\1\t\2\t\3\t\5\n', line))
+in_file = open(sys.argv[1], 'r')
+content = in_file.read()
+in_file.close()
 
-print(f0_lines)
+for match in r.finditer(content):
+    print(match)
 
+# content = r.sub(r'\1\t\2\t\3\t\5\n', content)
+# print(content)
+
+out_file = open(sys.argv[2], 'w')
+out_file.write(content)
+out_file.close()
--- test/run_train_tests.sh
+++ test/run_train_tests.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-for i in {1..3}; do
-    docker run -v $(pwd):/var/loadtest --net host -it --rm direvius/yandex-tank -c load/load_$i.ini > log.tmp
-    grep -m 1 "Web link" log.tmp | cut -d" " -f5
-    grep -m 1 "Artifacts dir" log.tmp | tr -d "\r" | cut -d" " -f5 | cut -d"/" -f4 -f5 | xargs ls -1 | grep "answ_"
-done
+for i in {1..1}; do
+    docker run -v $(pwd):/var/loadtest --net host -i --rm direvius/yandex-tank -c load/load_$i.ini > log.tmp
+    url=$(grep -m 1 "Web link" log.tmp | cut -d" " -f5)
+    log_path=$(grep -m 1 "Artifacts dir" log.tmp | tr -d "\r" | cut -d" " -f5 | cut -d"/" -f4 -f5)
+    log_file=$(grep -m 1 "Artifacts dir" log.tmp | tr -d "\r" | cut -d" " -f5 | cut -d"/" -f4 -f5 | xargs ls -1 | grep "answ_")
+    log="$log_path/$log_file"
 
-#rm -f log.tmp
+    python ./check/prepare.py log results/tmp
+    echo url > results/result_$1.log
+    echo results/tmp >> results/result_$1.log
+    rm -f results/tmp
+done
